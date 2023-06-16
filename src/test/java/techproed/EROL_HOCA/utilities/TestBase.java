1722feaacd6c22e523e3a92c9cfdf637bc633d10
AydaAkçay <aydaakcay@gmail.com>
2023-06-16 11:20:23 +0300

    16/06/23

--- src/test/java/techproed/EROL_HOCA/utilities/TestBase.java
+++ src/test/java/techproed/EROL_HOCA/utilities/TestBase.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class TestBase {
 
@@ -78,4 +80,18 @@ public abstract class TestBase {
             Select select = new Select(ddm);
             select.selectByValue(value);
         }
+
+
+
+
+    //SwitchTo Window-1
+    public void switchToWindow(int index){
+        List<String> pencereler = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(pencereler.get(index));
     }
+    //SwitchTo Window-2
+    public void switchToWindow2(int index){
+        driver.switchTo().window(driver.getWindowHandles().toArray()[index].toString());
+    }
+
+}
