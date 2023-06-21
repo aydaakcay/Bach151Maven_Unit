a1738433c6d419f345f2b12203969e7eac206e86
AydaAkçay <aydaakcay@gmail.com>
2023-06-21 13:52:48 +0300

    21/06/

--- src/test/java/techproed/EROL_HOCA/utilities/TestBase.java
+++ src/test/java/techproed/EROL_HOCA/utilities/TestBase.java
@@ -3,10 +3,14 @@ package techproed.EROL_HOCA.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -44,6 +48,29 @@ public abstract class TestBase {
             }
         }
 
+
+    //Selenium Wait/Explicit Wait
+    //visibilityOf(element) methodu
+    public void visibleWait(WebElement element,int saniye){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(saniye));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    //visibilityOfElementLocated(locator) methodu
+    public void visibleWait(By locator, int saniye){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(saniye));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+
+    //AlertWait methodu
+    public void alertWait(int saniye){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(saniye));
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+
+
         //AcceptAlert
         public void acceptAlert(){
             driver.switchTo().alert().accept();
@@ -68,6 +95,16 @@ public abstract class TestBase {
             Select select = new Select(ddm);
             select.selectByVisibleText(text);
         }
+
+
+    //FluentWait visible Methodu
+    public void visibleFluentWait(WebElement element,int saniye,int milisaniye){
+        new FluentWait<>(driver).withTimeout(Duration.ofSeconds(saniye)).
+                pollingEvery(Duration.ofMillis(milisaniye)).
+                until(ExpectedConditions.visibilityOf(element));
+    }
+
+
 
         //DropDown Index
         public void selectIndex(WebElement ddm,int index){
